a89792ca29d6e9f80eebef07b425d064ede9dad4
rishabhtatia1 <rishabhtatia1@gmail.com>
2019-08-01 10:16:03 +0530

    Cart Update

--- PizzaDeliveryApp/script.js
+++ PizzaDeliveryApp/script.js
@@ -1,13 +1,13 @@
 const cartBtn = document.querySelectorAll('.pizzaButton');
 
 cartBtn.forEach(function(item, key) {
-  item.addEventListener('click', function() {
-    document.getElementById("cartWrapper").style.display = "block";
-  });
+    item.addEventListener('click', function() {
+        document.getElementById("cartWrapper").style.display = "block";
+    });
 });
 
 const closeBtn = document.querySelector('#closeCart');
 
-closeBtn.addEventListener('click', function () {
-  document.getElementById("cartWrapper").style.display = "none";
+closeBtn.addEventListener('click', function() {
+    document.getElementById("cartWrapper").style.display = "none";
 });
